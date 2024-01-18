9661fbde91f6b66fb571e07a6635445fcd606f85
cbfranco04 <cbfranco@mit.edu>
2024-01-18 18:55:34 -0500

    Added LobbySelect Containers

--- client/src/components/pages/SelectLobby.js
+++ client/src/components/pages/SelectLobby.js
@@ -1,9 +1,14 @@
 import React from "react";
+import "./SelectLobby.css";
 const SelectLobby = () => {
   return (
     <>
-      <div className="text-4xl text-primary-text font-bold bg-primary-bg w-full h-full min-h-screen px-4 py-2">
-        hi
+      <div className="text-4xl text-center font-bold bg-primary-bg w-full h-full min-h-screen px-4">
+        <h1 className="text-primary-text py-8">Lobby Select</h1>
+        <div className="LobbyContainerOuter">
+          <div className="LobbyContainerInner">
+          </div>
+        </div>
       </div>
     </>
   );
