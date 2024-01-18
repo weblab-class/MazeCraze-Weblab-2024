df5b37c0582e26205b34bec41c6250c123ef26e8
chicken-04 <datctran2005@gmail.com>
2024-01-18 14:20:03 -0500

    Added Hover Line Animation in Home Page

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -7,13 +7,13 @@ const Home = () => {
       <div className="text-4xl text-primary-text font-bold bg-primary-bg w-full h-full min-h-screen px-4 py-2">
         <div className="absolute top-2 left-4">Maze Craze</div>
         <div className="flex flex-col w-full h-screen items-center justify-center">
-          <div className="gap-3 flex flex-col text-lef">
+          <div className="gap-1 flex flex-col text-left">
             {/* Eventually Change These Paragraphs into Link Componenets */}
 
-            <p>Find Game</p>
-            <p>Customize</p>
-            <p>Tutorial</p>
-            <p>Logout</p>
+            <p className="pb-2">Find Game</p>
+            <p className="pb-2">Customize</p>
+            <p className="pb-2">Tutorial</p>
+            <p className="pb-2">Logout</p>
           </div>
         </div>
       </div>
