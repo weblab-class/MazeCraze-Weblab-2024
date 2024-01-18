eb1f49e9ecc4d3ebdc9e0664861b91b241307f56
chicken-04 <datctran2005@gmail.com>
2024-01-18 13:18:47 -0500

    Keys Added

--- client/src/components/pages/Skeleton.js
+++ client/src/components/pages/Skeleton.js
@@ -4,8 +4,7 @@ import { GoogleOAuthProvider, GoogleLogin, googleLogout } from "@react-oauth/goo
 import "../../utilities.css";
 import "./Skeleton.css";
 
-//TODO: REPLACE WITH YOUR OWN CLIENT_ID
-const GOOGLE_CLIENT_ID = "FILL ME IN";
+const GOOGLE_CLIENT_ID = "111532973746-seocmk0fff7s6feauriigvt11359c5td.apps.googleusercontent.com";
 
 const Skeleton = ({ userId, handleLogin, handleLogout }) => {
   return (
@@ -22,7 +21,7 @@ const Skeleton = ({ userId, handleLogin, handleLogout }) => {
       ) : (
         <GoogleLogin onSuccess={handleLogin} onError={(err) => console.log(err)} />
       )}
-      <h1>Good luck on your project :)</h1>
+      {/* <h1>Good luck on your project :)</h1>
       <h2> What you need to change in this skeleton</h2>
       <ul>
         <li>
@@ -39,7 +38,7 @@ const Skeleton = ({ userId, handleLogin, handleLogout }) => {
       <h2>How to go from this skeleton to our actual app</h2>
       <a href="https://docs.google.com/document/d/110JdHAn3Wnp3_AyQLkqH2W8h5oby7OVsYIeHYSiUzRs/edit?usp=sharing">
         Check out this getting started guide
-      </a>
+      </a> */}
     </GoogleOAuthProvider>
   );
 };
--- server/auth.js
+++ server/auth.js
@@ -3,8 +3,7 @@ const User = require("./models/user");
 const socketManager = require("./server-socket");
 
 // create a new OAuth client used to verify google sign-in
-//    TODO: replace with your own CLIENT_ID
-const CLIENT_ID = "FILL IN CLIENT ID";
+const CLIENT_ID = "111532973746-seocmk0fff7s6feauriigvt11359c5td.apps.googleusercontent.com";
 const client = new OAuth2Client(CLIENT_ID);
 
 // accepts a login token from the frontend, and verifies that it's legit
