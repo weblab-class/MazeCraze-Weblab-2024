0eca2ddbc7478c37b10740bf87eb63d205381d40
r-ejilemele <r.ejilemele@gmail.com>
2024-01-19 20:36:28 -0500

    made the login button span the whole page and be invisible

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Dashboard from "./Dashboard.js";
 import SignIn from "./SignIn.js";
-const Home = ({handleLogin, handleLogout, userId, isLoggedIn}) => {
+const Home = ({handleLogin, handleLogout, userId, isLoggedIn, setIsLoggedIn}) => {
   return (
     <>
       {
       
         isLoggedIn ? <Dashboard userId={userId} handleLogout={handleLogout}/> : 
-        <SignIn userId={userId} handleLogin={handleLogin} handleLogout={handleLogout}/>
+        <SignIn userId={userId} handleLogin = {handleLogin} handleLogout = {handleLogout} isLoggedIn = {isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
 
       }
     </>
