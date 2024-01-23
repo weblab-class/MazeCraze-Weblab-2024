f3b0bffa1ff5acb3b44373dfd4a7e63ec1f1f9da
chicken-04 <datctran2005@gmail.com>
2024-01-23 16:30:13 -0500

    Update SingleLobby.js

--- client/src/components/modules/SingleLobby.js
+++ client/src/components/modules/SingleLobby.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const SingleLobby = ({ lobby }) => {
   return (
-    <div className="p-10 bg-white text-primary-text">
+    <div className="p-10 text-primary-text">
       Lobby Amount:{lobby.user_ids.length} <br /> LobbyID: {lobby.lobby_id} <br />
     </div>
   );
