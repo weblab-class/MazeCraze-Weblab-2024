bb91f5e9f1d7196033ae7b999e1a002d2d679427
r-ejilemele <r.ejilemele@gmail.com>
2024-01-23 18:42:16 -0500

    Made design for Customize pagge

--- client/src/components/pages/Customize.js
+++ client/src/components/pages/Customize.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 import { redirect, useNavigate, useLocation } from "react-router-dom";
+import CustomizeBackground from "../../public/images/SettingsBackground.svg";
+
 
 const Customize = (props) => {
   const location = useLocation();
@@ -12,15 +14,23 @@ const Customize = (props) => {
   //   }
   // }, [loading]);
   return (
-    <>
-      {!props.userId ? (
-        redirect("/")
-      ) : (
-        <div className="text-4xl text-primary-text font-bold bg-primary-bg w-full h-full min-h-screen px-4 py-2 font-custom tracking-widest">
-          Customize
-        </div>
-      )}
-    </>
+   
+    <div class="bg-primary-pink h-screen w-full relative flex flex-col justify-center items-center text-primary-text font-custom tracking-widest">
+        Customize
+    
+    <div class='h-[35%] w-[20%] absolute top-0 right-0 bg-primary-bg'></div>
+  
+    <div class='h-[35%] w-[20%] absolute left-0 bottom-0 bg-primary-bg'></div>
+    <div class="h-[15%] flex justify-center items-center text-6xl absolute top-0 w-full text-primary-bg">
+      Customize
+    </div>
+
+    <div class='h-[70%] w-[85%] absolute bg-primary-block text-4xl px-3 py-2'>
+      KeyBinds
+    </div>
+
+  
+  </div>
   );
 };
 
