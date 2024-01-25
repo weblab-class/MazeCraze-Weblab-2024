15c641bee43be39fbeecc611f5db4b83248e5577
r-ejilemele <r.ejilemele@gmail.com>
2024-01-25 15:54:29 -0500

    added back button to GameLobby

--- client/src/components/pages/GameLobby.js
+++ client/src/components/pages/GameLobby.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { get } from "../../utilities.js";
 import { Link, useNavigate } from "react-router-dom";
+import { IoArrowBackCircle } from "react-icons/io5";
+import { IoArrowBackCircleOutline } from "react-icons/io5";
 
 import { socket } from "../../client-socket.js";
 import LobbyUserCard from "../modules/LobbyUserCard.js";
@@ -8,7 +10,18 @@ const GameLobby = ({ lobbyId, userId }) => {
   const [lobby, setLobby] = useState({});
   const [lobbyUsers, setLobbyUsers] = useState([]);
   const [isHost, setIsHost] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+
   const navigate = useNavigate();
+  const navigateBack = () => {
+    navigate(-1);
+  };
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
 
   const setNewLobby = (data) => {
     console.log("LOBBY USERS ON SOCKET ", lobbyUsers.length);
@@ -44,17 +57,36 @@ const GameLobby = ({ lobbyId, userId }) => {
   }, []);
 
   return (
-    <div className="flex flex-col bg-primary-bg w-full h-full min-h-screen px-4 py-2 font-custom tracking-widest">
-      <div className="text-5xl text-primary-text font-bold mb-10">GameLobby</div>
-      <div className="flex gap-4 h-96">
-        <div className="w-full bg-white">
+    <div className="flex flex-col bg-primary-bg w-full h-full min-h-screen px-4 py-2 font-custom tracking-widest overflow-hidden">
+      <div className="text-5xl text-primary-text font-bold mb-10 relative">
+        {isHovered ? (
+              <IoArrowBackCircleOutline
+                onMouseOut={handleMouseLeave}
+                size={60}
+                onClick={navigateBack}
+                className="absolute left-0"
+              />
+            ) : (
+              <IoArrowBackCircle 
+              onMouseOver={handleMouseEnter} 
+              size={60} 
+              onClick={navigateBack} 
+              className="absolute left-0"
+              />
+            )}
+        <div className=" h-full min-w-screen w-full flex justify-center text-center absolute inset-y-2">
+          GameLobby
+        </div>
+      </div>
+      <div className="flex gap-4 h-96 w-full mt-9">
+        <div className="w-full bg-white p-3">
           Player Box
           {lobbyUsers.map((user, i) => (
             <LobbyUserCard data={user} key={i} />
           ))}
         </div>
         <div className="bg-blue-200 w-full">
-          <div className="text-2xl text-primary-text font-bold mb-5 text-center">
+          <div className="text-2xl text-primary-text font-bold mb-5 text-center pt-3">
             Welcome to Room {lobbyId}
           </div>
           <div className="flex flex-col px-10 text-wrap text-xl ">
