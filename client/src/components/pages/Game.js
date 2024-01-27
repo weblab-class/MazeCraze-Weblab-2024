931e2793ae1680588c10c3d69eeb38497c9475c8
chicken-04 <datctran2005@gmail.com>
2024-01-27 16:57:38 -0500

    FINISHED BETWEENPAGE

--- client/src/components/pages/Game.js
+++ client/src/components/pages/Game.js
@@ -1,50 +1,60 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./Game.css";
 import Maze from "../modules/Maze.js";
-import Timer from "../modules/Timer.js"
+import Timer from "../modules/Timer.js";
 import BetweenRound from "./BetweenRound.js";
-import {socket} from "../../client-socket";
+import { socket } from "../../client-socket";
+import { get } from "../../utilities.js";
 
+const Game = ({ lobbyId, userId }) => {
+  const [isBetweenRound, setIsBetweenRound] = useState(false);
+  const [lobbyGameState, setLobbyGameState] = useState({ playerStats: {} });
 
-const Game = ({lobbyId, userId}) => {
+  useEffect(() => {
+    socket.on("EndRound", (lobbyGameState) => {
+      setLobbyGameState(lobbyGameState); // TO DO, MAKE IT RECEIVE MULTIPLE PLAYER COINS
+      setIsBetweenRound(true);
+    });
+    return () =>
+      socket.off("EndRound", (lobbyGameState) => {
+        setLobbyGameState(lobbyGameState); // TO DO, MAKE IT RECEIVE MULTIPLE PLAYER COINS
+        setIsBetweenRound(true);
+      });
+  }, []);
 
-    const [isBetweenRound, setIsBetweenRound] = useState(false);
-    const [lobbyGameState, setLobbyGameState] = useState({});
-
-    useEffect(() => {
-        socket.on("EndRound", (lobbyGameState) => {
-            setLobbyGameState(lobbyGameState) // TO DO, MAKE IT RECEIVE MULTIPLE PLAYER COINS
-            setIsBetweenRound(true);
-        });
-        return () => (
-            socket.off("EndRound")
-        );
-    },[]);
-
-    return (
-        <>
-            { isBetweenRound ? (
-                <BetweenRound lobbyGameState={lobbyGameState}/>
-            ):(
-                <div className="bg-primary-bg w-full h-full min-h-screen px-4 py-2 font-custom tracking-widest">
-                <h1 className="font-bold text-center text-4xl text-primary-text">Perks</h1>
-                <div className="PerkContainer">
-                        <div className="Perk" />
-                        <div className="Perk" />
-                        <div className="Perk" />
-                        <div className="Perk" />
-                        <div className="Perk" />
-                </div>
-                <div className="flex justify-center m-8 h-4/5">
-                    <Maze lobbyId={lobbyId} userId={userId}/>
-                </div>
-                <Timer />
-            </div>
-
-            )}
-        </>
-    )
-
-}
+  useEffect(() => {
+    get("/api/user_lobby", { lobby_id: lobbyId })
+      .then((data) => {
+        setLobbyGameState(data.lobbyGameState);
+      })
+      .catch((err) => console.log("Getting Lobby with Lobby Id In Game Given Has Error: ", err));
+  }, []);
+  return (
+    <BetweenRound lobbyGameState={lobbyGameState} />
+    // <>
+    //   <div className="bg-primary-bg w-full h-full min-h-screen">
+    //     {isBetweenRound ? (
+    //       <BetweenRound lobbyGameState={lobbyGameState} />
+    //     ) : (
+    //       <>
+    //         <h1 className="font-bold text-center text-4xl text-primary-text">Perks</h1>
+    //         {/* Eventually Make Perks an Individual Componenet */}
+    //         <div className="PerkContainer">
+    //           <div className="Perk" />
+    //           <div className="Perk" />
+    //           <div className="Perk" />
+    //           <div className="Perk" />
+    //           <div className="Perk" />
+    //         </div>
+    //         <div className="flex justify-center m-8 h-4/5">
+    //           <Maze lobbyId={lobbyId} userId={userId} />
+    //         </div>
+    //         <Timer />
+    //       </>
+    //     )}
+    //   </div>
+    // </>
+  );
+};
 
 export default Game;
