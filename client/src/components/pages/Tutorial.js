c67b1751cd8baa3671e0bd6edf5a43d056df1212
r-ejilemele <r.ejilemele@gmail.com>
2024-01-29 21:41:50 -0500

    Did Tutorial Page

--- client/src/components/pages/Tutorial.js
+++ client/src/components/pages/Tutorial.js
@@ -1,9 +1,8 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { IoArrowBackCircle } from "react-icons/io5";
 import { IoArrowBackCircleOutline } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
 import { get, post } from "../../utilities.js";
-
 
 const Tutorial = () => {
   const [isHovered, setIsHovered] = useState(false);
@@ -19,9 +18,13 @@ const Tutorial = () => {
     navigate("/");
   };
 
-  useEffect(()=>{
-    get("/api/user").then(()=>{}).catch(()=>{navigate("/")})
-  },[])
+  useEffect(() => {
+    get("/api/user")
+      .then(() => {})
+      .catch(() => {
+        navigate("/");
+      });
+  }, []);
 
   return (
     <div className="text-4xl font-custom tracking-widest text-primary-text font-bold bg-primary-bg w-full h-full min-h-screen px-4 overflow-hidden flex justify-center items-center relative ">
@@ -35,24 +38,62 @@ const Tutorial = () => {
           />
         ) : (
           <IoArrowBackCircle
-          onMouseOver={handleMouseEnter}
-          size={60}
-          onClick={navigateBack}
-          className="absolute left-0 z-50"
+            onMouseOver={handleMouseEnter}
+            size={60}
+            onClick={navigateBack}
+            className="absolute left-0 z-50"
           />
         )}
         Tutorial
       </div>
-      <div className="bg-primary-block h-[60%] w-[88%] flex absolute p-8">
-        <div className="bg-primary-pink h-full w-full z-50 flex justify-center items-center text-primary-bg text-xl p-5 text-wrap text-center">
-          Welcome to Maze Craze. 
-          <br /> To play the game is simple. 
-          <br /> Use the keys specified in Customize page to navigate your character. 
-          <br /> The goal of the game is to win the game through collecting coins.  
-          <br />Coins are worth points each.
-          <br />After every round a perk will be picked at random that will an effect on the gameplay. That perk will be displayed at the top
-          <br />The perks include HERMES BOOTS, SOCIAL DISTANCING, 
-          <br /> Have fun and enjoy the chaos!
+      <div className="bg-primary-block h-[70%] w-[88%] flex absolute p-8">
+        <div className="bg-primary-pink h-full w-full z-50 flex  items-center flex-col text-primary-bg text-xl p-5 text-wrap text-center overflow-auto">
+          <div className="text-3xl">Welcome to Maze Craze!</div>
+          <br />
+          <br /> To play the game is simple.
+          <br /> Use the keys specified in Customize page to navigate your character.
+          <br /> The goal of the game is to win through collecting coins.
+          <br />
+          After every round a perk will be picked at random that will have an effect on the
+          gameplay. That perk will be displayed at the top.
+          <br />
+          <br />
+          The perks include CRUMBLING WALL, HERMES BOOTS, HYDRA COINS, MAZE HAZE, SOCIAL DISTANCING,
+          THREE BLIND MICE, AND WANDERING COINS
+          <br />
+          <br />
+          <div className="">
+            <br />
+            <div className=" text-xl text-primary-block">Crumbling Walls </div>
+            perk will remove one wall every second.
+            <br />
+            <br />
+            <div className=" text-xl text-primary-block">Hydra Coins </div>
+            perk has a chance to spawn an additional coin after the user collects a coin.
+            <br />
+            <br />
+            <div className=" text-xl text-primary-block">Hermes Boots</div>
+            perk will increase movement speed.
+            <br />
+            <br />
+            <div className="text-xl text-primary-block">Maze Haze</div>
+            perk will limit field of view
+            <br />
+            <br />
+            <div className="text-xl text-primary-block">Social Distancing</div>
+            perk will cause players to die when they crash into each other
+            <br />
+            <br />
+            <div className="text-xl text-primary-block">Three Blind Mice</div>
+            perk spawn three mice that will chase players down and try to kill them
+            <br />
+            <br />
+            <div className="text-xl text-primary-block">Wandering Coins</div>
+            perk will make Coins move around the map
+          </div>
+          <br />
+          <br />
+          <div className="text-4xl">Have fun and enjoy the chaos!</div>
         </div>
       </div>
       <div className="bg-primary-block h-full w-20 flex absolute left-10"></div>
