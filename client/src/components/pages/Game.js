ebbdb5e4924f0b31c724dd90d43652c549e8b36b
chicken-04 <datctran2005@gmail.com>
2024-01-31 00:12:04 -0500

    Finished Images

--- client/src/components/pages/Game.js
+++ client/src/components/pages/Game.js
@@ -5,29 +5,22 @@ import Timer from "../modules/Timer.js";
 import BetweenRound from "./BetweenRound.js";
 import { socket } from "../../client-socket";
 import { get } from "../../utilities.js";
-import { useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import FinishedGame from "./FinishedGame.js";
 import { round_time, max_rounds } from "../modules/constants.js";
-import CrumblingWall from "../../../dist/perkPictures/Crumbling_wall.png";
-import Hermes from "../../../dist/perkPictures/Hermes_Boots.png";
-import Hydra from "../../../dist/perkPictures/Hydra.png";
-import MazeHaze from "../../../dist/perkPictures/Maze_Haze.png";
-import ThreeBlindMice from "../../../dist/perkPictures/Three_Blind_Mice.png";
-import WanderingCoins from "../../../dist/perkPictures/Wandering_Coins.png";
-import SocialDistancing from "../../../dist/perkPictures/Social_Distancing.png";
-
+import perkDict from "../modules/constants.js";
 const Game = ({ lobbyId, userId }) => {
   const [isBetweenRound, setIsBetweenRound] = useState(false);
   const [lobbyGameState, setLobbyGameState] = useState({ playerStats: {} });
   const [gameFinished, setGameFinished] = useState(false);
   const [time, setTime] = useState(round_time);
-  const [activatedPerks, setActivatedPerks] = useState([])
-  const navigate = useNavigate()
+  const [activatedPerks, setActivatedPerks] = useState([]);
+  const navigate = useNavigate();
 
   const round_update = (data) => {
     console.log("ROUND DATA", data);
     setLobbyGameState(data.lobbyGameState);
-    setActivatedPerks(data.lobbyGameState.activatedPerks)
+    setActivatedPerks(data.lobbyGameState.activatedPerks);
     setIsBetweenRound(!data.lobbyGameState.in_round);
     setGameFinished(data.lobbyGameState.round > max_rounds);
   };
@@ -37,11 +30,11 @@ const Game = ({ lobbyId, userId }) => {
   }, []);
 
   useEffect(() => {
-    window.addEventListener('beforeunload', (e) => {
+    window.addEventListener("beforeunload", (e) => {
       socket.emit("removeUserFromGame", { userId: userId, lobbyId: lobbyId });
     });
     return () => {
-      window.removeEventListener('beforeunload', (e) => {
+      window.removeEventListener("beforeunload", (e) => {
         socket.emit("removeUserFromGame", { userId: userId, lobbyId: lobbyId });
       });
     };
@@ -62,21 +55,20 @@ const Game = ({ lobbyId, userId }) => {
         setTime(data.timeLeft);
       });
   }, []);
-  
+
   useEffect(() => {
     get("/api/user_lobby", { lobby_id: lobbyId })
       .then((data) => {
         get("/api/user").then((data2) => {
-        if(data.lobbyGameState) {
-
-          setLobbyGameState(data.lobbyGameState);
-          if(!Object.keys(data.lobbyGameState.playerStats).includes(data2.user._id)) {
-            navigate("/")
+          if (data.lobbyGameState) {
+            setLobbyGameState(data.lobbyGameState);
+            if (!Object.keys(data.lobbyGameState.playerStats).includes(data2.user._id)) {
+              navigate("/");
+            }
+          } else {
+            navigate("/");
           }
-        } else {
-          navigate("/");
-        }
-        })
+        });
       })
       .catch((err) => console.log("Getting Lobby with Lobby Id In Game Given Has Error: ", err));
   }, []);
@@ -84,37 +76,24 @@ const Game = ({ lobbyId, userId }) => {
   return (
     <div>
       {/* <BetweenRound lobbyGameState={lobbyGameState} /> */}
-      {isBetweenRound ? (
+      {isBetweenRound || !isBetweenRound ? (
         <BetweenRound lobbyGameState={lobbyGameState} timer={time} />
-      ) : (gameFinished) ? (
+      ) : gameFinished ? (
         <FinishedGame lobbyGameState={lobbyGameState} />
       ) : (
         <div className="bg-primary-bg w-full h-full min-h-screen">
           <h1 className="font-bold text-center text-2xl text-primary-text h-[3%]">Perks</h1>
           {/* Eventually Make Perks an Individual Componenet */}
           <div className="PerkContainer h-[10%]">
-            {activatedPerks.map((perk, i) =>{
-              if (perk === "Crumbling Walls"){
-                return <img className="Perk" src={CrumblingWall} key={i}/>
-              } else if (perk === "Hermes Boots") {
-                return <img className="Perk" src={Hermes} key={i}/>
-              } else if (perk === "Hydra Coins") {
-                return <img className="Perk" src={Hydra} key={i}/>
-              } else if (perk === "Maze Haze") {
-                return <img className="Perk" src={MazeHaze} key={i}/>
-              } else if (perk === "Social Distancing") {
-                return <img className="Perk" src={SocialDistancing} key={i}/>
-              } else if (perk === "Three Blind Mice") {
-                return <img className="Perk" src={ThreeBlindMice} key={i}/>
-              } else if (perk === "Wandering Coins") {
-                return <img className="Perk" src={WanderingCoins} key={i}/>
-              }
+            {activatedPerks.map((perk, i) => {
+              return (
+                <img
+                  className="Perk border-4 border-primary-text"
+                  key={i}
+                  src={perkDict[perk].src}
+                />
+              );
             })}
-            {/* <img className="Perk" src={}/>
-            <img className="Perk" />
-            <img className="Perk" />
-            <img className="Perk" />
-            <img className="Perk" /> */}
           </div>
           <div className="flex justify-center m-8 h-[80%]">
             <Maze lobbyId={lobbyId} userId={userId} />
